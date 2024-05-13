90a23ceff86cdca554d181a0ec80bf92b5a1ceb6
Camila Serra <66450896+camilaserra@users.noreply.github.com>
2024-05-13 18:57:06 -0300

    feat: Add fade animation to SwiperHero component

--- src/components/Swipers/SwiperHero.tsx
+++ src/components/Swipers/SwiperHero.tsx
@@ -6,6 +6,8 @@ import { Pagination, Autoplay } from 'swiper'
 import { Container } from '../Partials/Container'
 import { client } from '@/services/prismicClient'
 import useSWR from 'swr'
+import { Fade } from "react-awesome-reveal";
+
 
 export const fetchCache = 'force-no-store'
 export const dynamic = 'force-dynamic'
@@ -28,8 +30,12 @@ export default function SwiperHero() {
         <SwiperSlide key={index} style={{ backgroundImage: `url(${imagem.imagem.url})` }} className="bg-cover bg-center bg-no-repeat flex items-center justify-start relative bg-black">
           <Container>
             <div className='flex flex-col justify-center items-center relative z-50 mt-10 md:mt-0'>
-              <p className="text-white text-3xl mb-2 md:text-5xl font-normal relative trajan uppercase">{imagem.titulo}</p>
-              <h2 className="text-white text-2xl md:text-3xl lg:text-5xl font-bold relative text-center uppercase max-w-[90%] md:max-w-[70%] barlow">{imagem.subtitulo}</h2>
+              <Fade duration={2000}>       
+                <p className="text-white text-3xl mb-2 md:text-5xl font-normal relative trajan uppercase">{imagem.titulo}</p>
+              </Fade>
+              <Fade duration={3000} className='max-w-[90%] md:max-w-[70%]'>       
+                <h2 className="text-white text-2xl md:text-3xl lg:text-5xl font-bold relative text-center uppercase barlow">{imagem.subtitulo}</h2>
+              </Fade>
             </div>
           </Container>
           <img src="/img/elements/element-01-gray.png" alt="Imagem element white" className='absolute bottom-0 z-40 hidden md:block' />
